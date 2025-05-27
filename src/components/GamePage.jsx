355197d5dc6b436a9ae611bcd69d4a698acb6fa0
sehajini1 <jayakodyjast@uom.lk>
2025-05-28 01:11:45 +0530

    added instructions

--- src/components/GamePage.jsx
+++ src/components/GamePage.jsx
@@ -80,10 +80,10 @@ export default function GamePage(){
 
     return(
         <div className="min-h-screen bg-blue-100 flex flex-col items-center pt-10 overflow-hidden">
-            <h1 className="text-4xl font-bold mb-10 text-gray-800">Connect Four hello</h1>
+            <h1 className="text-4xl font-bold mb-10 text-gray-800">Connect Four</h1>
 
             <div className="flex flex-row w-full max-w-7xl gap-6 px-4">
-                <div className="w-1/4 min-w-[200px] bg-red-500">
+                <div className="w-1/4 min-w-[200px]">
                     <InstructionsComponent />
                 </div>
 
